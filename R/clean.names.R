c08e2c9c94d92f9b604ae67935ece0d5bfede548
rBatt <battrd@gmail.com>
2015-10-20 21:26:42 -0400

    clean names

--- R/clean.names.R
+++ R/clean.names.R
@@ -114,8 +114,8 @@ clean.names.gmex <- function(X){
 		"VESSEL" = "vessel",
 		"BIO_BGS" = "SID",
 		"STATIONID" = "station",
-		"CRUISE_NO" = "cruise", # don't know how this is different from CRUISEID
-		"P_STA_NO" = "station", # don't know how it's different from stationid
+		# "CRUISE_NO" = "cruise", # don't know how this is different from CRUISEID
+		# "P_STA_NO" = "station", # don't know how it's different from stationid
 		"BGSID" = "BGSID",
 		"CATEGORY" = "unknown",
 	
@@ -132,31 +132,31 @@ clean.names.gmex <- function(X){
 		"GEAR_TYPE" = "geartype",
 		"MESH_SIZE" = "meshsize",
 		# "OP" = "status of the tow; blank is good, so is a '.'; all else bad (see filedef.doc)",
-		"MIN_FISH" = "duration",
+		"MIN_FISH" = "towduration",
 	
 		"TIME_ZN"="timezone",
 		"TIME_MIL"="time",
 	
 	
-		"S_LATD"=paste("starting", "lat.deg",sep="."),
-		"S_LATM"=paste("starting", "lat.min",sep="."),
-		"S_LOND"=paste("starting", "lon.deg",sep="."),
-		"S_LONM"=paste("starting", "lon.min",sep="."),
+		"S_LATD"= "lat.deg.start", #paste("start", "lat.deg",sep="."),
+		"S_LATM"= "lat.min.start", #paste("start", "lat.min",sep="."),
+		"S_LOND"= "lon.deg.start", # paste("start", "lon.deg",sep="."),
+		"S_LONM"= "lon.min.start", # paste("start", "lon.min",sep="."),
 	
-		"DEPTH_SSTA" = paste("starting", "depth2",sep="."),
+		"DEPTH_SSTA" = "depth2.start", # paste("start", "depth2",sep="."),
 		"MO_DAY_YR" = "date",
 	
-		"E_LATD"=paste("ending", "lat.deg",sep="."),
-		"E_LATM"=paste("ending", "lat.min",sep="."),
-		"E_LOND"=paste("ending", "lon.deg",sep="."),
-		"E_LONM"=paste("ending", "lon.min",sep="."),
+		"E_LATD"= "lat.deg.end", # paste("end", "lat.deg",sep="."),
+		"E_LATM"= "lat.min.end", # paste("end", "lat.min",sep="."),
+		"E_LOND"= "lon.deg.end", # paste("end", "lon.deg",sep="."),
+		"E_LONM"= "lon.min.end", # paste("end", "lon.min",sep="."),
 	
 	
 		"TEMP_SSURF" = "stemp",
 		"TEMP_BOT" = "btemp",
 	
 		"VESSEL_SPD"="towspeed",
-		"COMSTAT"= "station.comments",
+		"COMSTAT"= "station.comment",
 		"TAXONOMIC" = "spp",
 		"TITLE" = "survey.name"
 	)
@@ -242,7 +242,50 @@ clean.names.neus <- function(X){
 clean.names.newf <- function(X){
 	
 	
+	newf.cols <- c(
+		# "sppcode",
+# 		"recordtype",
+# 		"vessel",
+# 		"trip",
+# 		"set",
+# 		"yearl",
+# 		"monthl",
+# 		"dayl",
+# 		"settype",
+# 		"stratum",
+# 		"nafo",
+# 		"unitarea",
+# 		"light",
+# 		"winddir",
+# 		"windforce",
+# 		"sea",
+# 		"bottom",
+# 		"timel",
+		"duration" = "towduration", 
+		"distance" = "towdistance", 
+		# "operation",
+# 		"depth",
+		"depthmin" = "depth.min",
+		"depthmax" = "depth.max",
+# 		"depthbottom",
+		"surftemp" = "stemp", 
+		"bottemp" = "btemp", 
+		"latstart" = "lat.start", 
+		"lonstart" = "lon.start", 
+		# "posmethod",
+		"gear" = "geartype", 
+		"num" = "cnt", 
+		"wgt" = "weight", 
+		"latend" = "lat.end", 
+		"lonend" = "lon.end"#, 
+		# "bottempmeth",
+# 		"geardevice",
+# 		"common",
+# 		"spp",
+# 		"season"
+	)
 	
+	return(list(old=names(newf.cols), new=unname(newf.cols)))
 	
 }
 
@@ -251,6 +294,8 @@ clean.names.newf <- function(X){
 # =========
 clean.names.ngulf <- function(X){
 	message("Function not ready yet")
+	
+	return(list(old=NULL, new=NULL))
 }
 
 
@@ -259,17 +304,113 @@ clean.names.ngulf <- function(X){
 # ======
 clean.names.sa <- function(X){
 	
+	sa.cols <- c(
+		"COLLECTIONNUMBER" = "haulid", 
+		# "PROJECTNAME",
+		# "PROJECTAGENCY",
+		"DATE" = "date", 
+		# "EVENTNAME",
+		"VESSELNAME" = "vessel", 
+		# "GEARNAME",
+# 		"GEARCODE",
+# 		"SPECIESCODE",
+# 		"MRRI_CODE",
+		"SPECIESSCIENTIFICNAME" = "spp", 
+		"SPECIESCOMMONNAME" = "common", 
+		"NUMBERTOTAL" = "cnt", 
+		"SPECIESTOTALWEIGHT" = "weight", 
+		# "SPECIESSUBWEIGHT", 
+		# "SPECIESWGTPROCESSED",
+		# "WEIGHTMETHODDESC", 
+		# "ORGWTUNITS", 
+		"EFFORT" = "effort", 
+		# "CATCHSUBSAMPLED", 
+		# "CATCHWEIGHT", 
+		# "CATCHSUBWEIGHT", 
+		"TIMESTART" = "time", 
+		"DURATION" = "towduration", 
+		# "TOWTYPETEXT",
+		# "LOCATION",
+		# "REGION",
+		# "DEPTHZONE",
+		# "ACCSPGRIDCODE",
+		# "STATIONCODE",
+		# "EVENTTYPEDESCRIPTION",
+		"TEMPSURFACE" = "stemp", 
+		"TEMPBOTTOM" = "btemp", 
+		"SALINITYSURFACE" = "ssalin", 
+		"SALINITYBOTTOM" = "bsalin", 
+		"SDO" = "sdo", 
+		"BDO" = "bdo", 
+		"TEMPAIR" = "airtemp", 
+		"LATITUDESTART" = "lat.start", 
+		"LATITUDEEND" = "lat.end", 
+		"LONGITUDESTART" = "lon.start", 
+		"LONGITUDEEND" = "lon.end", 
+		# "SPECSTATUSDESCRIPTION",
+		# "LASTUPDATED",
+		# "LIGHTPHASE",
+		"TIMEZONE" = "timezone", 
+		"DEPTHSTART" = "depth.start", 
+		"DEPTHEND" = "depth.end", 
+		# "PRESSURE",
+		# "WINDSPEED",
+		# "WINDDIRECTION",
+		# "WAVEHEIGHT",
+		# "PRECIPITATION",
+		# "ESTIMATEDLOC",
+		# "SEDSIZEDESC",
+		# "BTMCOMPDESC",
+		# "WEATHERDESC",
+		# "WATERLVLDESC",
+		# "ALTERATIONDESC",
+		# "ACTIVITYDESC",
+		# "NBRREP",
+		"COMMENTS" = "comment"
+	)
 	
-
+	return(list(old=names(sa.cols), new=unname(sa.cols)))
+	
 }
+
 
 # =========
 # = SGULF =
 # =========
 clean.names.sgulf <- function(X){
 	
-
+	sgulf.cols <- c(
+		# "stratum",
+		# "vessel",
+		# "cruise",
+		# "set",
+		# "year",
+		"species" = "species.code", 
+		"catch" = "cntcpue", 
+		"biomass" = "wtcpue", 
+		# "month",
+	# 	"day",
+	# 	"expt",
+	# 	"time",
+	# 	"temperature",
+		"depthst" = "depth.start", 
+		"depthend" = "depth.end", 
+		# "dtow",
+		# "depth",
+		"latitude" = "lat", 
+		"longitude" = "lon", 
+		"latin_name" = "spp", 
+		"name" = "common", 
+		"N" = "cnt", 
+		"kg" = "weight", 
+		"t_surface" = "stemp", 
+		"salin_bottom" = "bsalin", 
+		"t_bottom" = "btemp", 
+		# "trawlableunits",
+		"stratarea" = "stratumarea"
+	)
 	
+	return(list(old=names(sgulf.cols), new=unname(sgulf.cols)))
 }
 
 # =========
@@ -277,8 +418,52 @@ clean.names.sgulf <- function(X){
 # =========
 clean.names.shelf <- function(X){
 	
-
+	shelf.cols <- c(
+		# "CODE",
+		# "stratum",
+		# "MISSION",
+		# "SETNO",
+		# "MARKET",
+		# "SAMPWGT",
+		"ADJ_TOTWGT" = "wtcpue",
+		"ADJ_TOTNO" = "cntcpue",
+		# "CALWT",
+		"REMARKS" = "comment",
+		# "SIZE_CLASS",
+		"SDATE" = "date",
+		"TIME" = "time",
+		"SLAT" = "lat.start",
+		"SLONG" = "lon.start",
+		"ELAT" = "lat.end",
+		"ELONG" = "lon.end",
+		"AREA" = "towarea",
+		"DUR" = "towduration",
+		"DIST" = "towdistance",
+		# "HOWD",
+		"SPEED" = "towspeed",
+		# "HOWS",
+		"DMIN" = "depth.min",
+		"DMAX" = "depth.max",
+		# "WIND",
+# 		"FORCE",
+# 		"CURNT",
+# 		"TYPE",
+		# "GEAR" = "geartype",
+		# "AUX",
+		"DEPTH" = "depth",
+		"ETIME" = "date.end",
+		"START_DEPTH" = "depth.start",
+		"END_DEPTH" = "depth.end",
+		"SURFACE_TEMPERATURE" = "stemp",
+		"BOTTOM_TEMPERATURE" = "btemp",
+		"BOTTOM_SALINITY" = "bsalin",
+		# "depthzone_fathoms",
+		"stratarea_nmi2" = "stratumarea"
+		# "spp",
+		# "common"
+	)
 	
+	return(list(old=names(shelf.cols), new=unname(shelf.cols)))
 }
 
 # ==========
@@ -306,7 +491,7 @@ clean.names.wcann <- function(X){
 		# "Best.Position.Type" = "The best trawl position (Best Latitude, Best Longitude) available has been provided in decimal degrees North Latitude and annotated with its type (Best Position Type).  Preference is given to any recorded gear position, usually an estimate of the onbottom midpoint: [(start_lat+end_lat)/2,  (start_lon+end_lon)/2].  If no acceptable onbottom gear position can be determined, a vessel position is provided, usually a similarly determined trawl midpoint estimate for the vessel.  If no acceptable vessel position can be determined, position data recorded for the defined station is provided.",
 		"Best.Depth..m." = "depth", 
 		# "Best.Depth.Type" = "how depth was determined",
-		"Trawl.Duration..min." = "duration", 
+		"Trawl.Duration..min." = "towduration", 
 		"Area.Swept.by.the.Net..hectares." = "areaswept", 
 		"Temperature.At.the.Gear..degs.C." = "btemp"
 	)
@@ -334,20 +519,22 @@ clean.names.wctri <- function(X){
 		# "HAUL_TYPE" = "type of haul",
 		# "PERFORMANCE" = "performance code of the haul",
 		"START_TIME" = "time",
-		"DURATION" = "duration", # hours?
+		"DURATION" = "towduration", # hours?
 		"DISTANCE_FISHED" = "towdistance", #"distance fished", # in km
 		"NET_WIDTH" = "gearsize", # m?
 		"STRATUM" = "stratum",
-		# "START_LATITUDE" = "latitude  at the start of the haul",
-		# "END_LATITUDE" = "latitude  at the end of the haul",
-		# "START_LONGITUDE" = "longitude  at the start of the haul",
-		# "END_LONGITUDE" = "longitude at the end of the haul",
+		"START_LATITUDE" = "lat.start",
+		"END_LATITUDE" = "lat.end",
+		"START_LONGITUDE" = "lon.start",
+		"END_LONGITUDE" = "lon.end",
 		"STATIONID" = "station", # no unit
 		"BOTTOM_DEPTH" = "depth",
 		"SURFACE_TEMPERATURE" = "stemp",
 		"GEAR_TEMPERATURE" = "btemp",
 		"SPECIES_NAME" = "spp",
-		"COMMON_NAME" = "common"
+		"COMMON_NAME" = "common",
+		"GEAR" = "geartype",
+		"time" = "datetime"
 	)
 	
 	return(list(old=names(wctri.cols), new=unname(wctri.cols)))

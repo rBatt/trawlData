78476c0e45d6122ae138eeecc07a0d60caa34434
rBatt <battrd@gmail.com>
2015-10-26 09:09:35 -0400

    improve efficiency & robustness of rmWhite & rm9s

--- R/format-value.R
+++ R/format-value.R
@@ -28,14 +28,16 @@ rmWhite <- function(x){
 	stopifnot(is.data.table(x))
 	nmx <- names(x)
 	classes <- sapply(x, class)
-	setClass <- classes%in%c("character","numeric","integer","integer64","logical") # leaving out factor b/c can't convert char to factor
+	setClass <- classes%in%c("character","numeric","integer","logical") # leaving out factor b/c can't convert char to factor #"integer64",
 	for(i in 1:ncol(x)){
 		t.name <- as.name(nmx[i])
 		if(setClass[i]){
-			expr <- bquote(.(t.name):=as(gsub("^\\s* | \\s*$", "", .(t.name)), Class=classes[i]))
+			# expr <- bquote(.(t.name):=as(gsub("^\\s* | \\s*$", "", .(t.name)), Class=classes[i]))
+			expr <- bquote(.(t.name):=as(str_trim(.(t.name)), Class=classes[i]))
 			x[,eval(expr)]
 		}else{
-			expr <- bquote(.(t.name):=gsub("^\\s* | \\s*$", "", .(t.name)))
+			# expr <- bquote(.(t.name):=gsub("^\\s* | \\s*$", "", .(t.name)))
+			expr <- bquote(.(t.name):=str_trim(.(t.name)))
 			x[,eval(expr)]
 		}
 		
@@ -50,8 +52,12 @@ rmWhite <- function(x){
 rm9s <- function(x){
 	stopifnot(is.data.table(x))
 	for(i in seq_along(x)){
-		set(x, i=which(x[[i]]==-9999L), j=i, value=as.character(NA))
-		set(x, i=which(x[[i]]==-9999.0), j=i, value=as.character(NA))
+		t.x <- x[[i]]
+		t.class <- class(t.x)
+		# set(x, i=which(x[[i]]==-9999L), j=i, value=as.character(NA))
+		# set(x, i=which(x[[i]]==-9999.0), j=i, value=as.character(NA))
+		
+		set(x, i=which(t.x==-9999L | t.x==-9999.0), j=i, value=as(NA,Class=t.class))
 	}
 }
 

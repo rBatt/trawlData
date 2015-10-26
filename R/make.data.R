17d7f547c2342c0c808cecc1169fe6075392f116
rBatt <battrd@gmail.com>
2015-10-26 19:00:38 -0400

    add clean.columns() step to data prep

--- R/make.data.R
+++ R/make.data.R
@@ -1,46 +1,57 @@
 ai <- read.trawl("ai")
 clean.names(ai, "ai")
 clean.format(ai, "ai")
+clean.columns(ai, "ai")
 
 ebs <- read.trawl("ebs")
 clean.names(ebs, "ebs")
 clean.format(ebs, "ebs")
+clean.columns(ebs, "ebs")
 
 gmex <- read.trawl("gmex")
 clean.names(gmex, "gmex")
 clean.format(gmex, "gmex")
+clean.columns(gmex, "gmex")
 
 goa <- read.trawl("goa")
 clean.names(goa, "goa")
 clean.format(goa, "goa")
+clean.columns(goa, "goa")
 
 neus <- read.trawl("neus")
 clean.names(neus, "neus")
 clean.format(neus, "neus")
+clean.columns(neus, "neus")
 
 newf <- read.trawl("newf")
 clean.names(newf, "newf")
 clean.format(newf, "newf")
+clean.columns(newf, "newf")
 
 sa <- read.trawl("sa")
 clean.names(sa, "sa")
 clean.format(sa, "sa")
+clean.columns(sa, "sa")
 
 sgulf <- read.trawl("sgulf")
 clean.names(sgulf, "sgulf")
 clean.format(sgulf, "sgulf")
+clean.columns(sgulf, "sgulf")
 
 shelf <- read.trawl("shelf")
 clean.names(shelf, "shelf")
 clean.format(shelf, "shelf")
+clean.columns(shelf, "shelf")
 
 wcann <- read.trawl("wcann")
 clean.names(wcann, "wcann")
 clean.format(wcann, "wcann")
+clean.columns(wcann, "wcann")
 
 wctri <- read.trawl("wctri")
 clean.names(wctri, "wctri")
 clean.format(wctri, "wctri")
+clean.columns(wctri, "wctri")
 
 
 cnames <- unique(c(names(ai), names(ebs), names(gmex), names(goa), names(neus), names(wcann), names(wctri)))

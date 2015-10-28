62fe458915a7380d4874ddbf385f601be1a31e9c
rBatt <battrd@gmail.com>
2015-10-27 21:08:27 -0400

    fix column names (matched2 doesn't exist anymore)

--- R/tax-grb.spp1.R
+++ R/tax-grb.spp1.R
@@ -6,20 +6,20 @@ countN <- function(x){ # count the number of times the letter "n" appears
 grb.spp1 <- function(x) {
 	tryCatch(
 		{
-			x <- x$results
-			x <- x[!duplicated(x[,"matched_name2"]),]
-			adjN <- pmax(countN(x$matched_name2) - countN(x$submitted_name), 0)*0.01 # gets bonus match score if the matched name has more n's, because n's appear to be missing a lot
+			# x <- x$results
+			x <- x[!duplicated(x[,"matched_name"]),]
+			adjN <- pmax(countN(x$matched_name) - countN(x$submitted_name), 0)*0.01 # gets bonus match score if the matched name has more n's, because n's appear to be missing a lot
 			x$score <- x$score + adjN
-			x <- x[max(which.max(x[,"score"]),1),c("submitted_name","matched_name2")]
-			if(x[,"matched_name2"]==""){x[,"matched_name2"] <- NA}
+			x <- x[max(which.max(x[,"score"]),1),c("submitted_name","matched_name")]
+			if(x[,"matched_name"]==""){x[,"matched_name"] <- NA}
 			return(x)
 		}, 
 		error=function(cond){
 			tryCatch(
 				{
-					data.frame(submitted_name=x$results[1, "submitted_name"], matched_name2=as.character(NA))
+					data.frame(submitted_name=x$results[1, "submitted_name"], matched_name=as.character(NA))
 				},
-				error=function(cond){data.frame(submitted_name=NA, matched_name2=NA)}
+				error=function(cond){data.frame(submitted_name=NA, matched_name=NA)}
 			)
 		}	
 	)

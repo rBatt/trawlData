52870cd2aa9205d1d458c5f2da42465e54c99edb
rBatt <battrd@gmail.com>
2015-11-03 17:18:50 -0500

    move check.consistent into own script

--- R/create.spp.key.R
+++ R/create.spp.key.R
@@ -170,71 +170,7 @@ create.spp.key <- function(spp, taxInfo, spp.corr1){
 	}
 	
 	
-	# =========================================
-	# = Check for and Correct Inconsistencies =
-	# =========================================
-	check.consistent <- function(Z, col2check=names(Z)[!names(Z)%in%c(by.col,not.consistent)], by.col="spp", not.consistent=c("ref","flag","val.src","tbl.row","match.src","tax.src")){
-		replacementsMade <- 0
-		replacementsFailed <- 0
-		replacementUnneeded <- 0
-		
-		taxProblemSolution <- data.table(spp=character(), prob.col=character(), solution=character())
-		tPS <- FALSE
-		
-		for(i in unique(Z[,eval(s2c(by.col))][[1]])){
-			for(j in col2check){
-				t.out <- Z[i,get(j)] # temporary value for a given `spp` and the j column
-				if(lu(t.out)>1){
-					if(lu(t.out[!is.na(t.out)])==1){ # if the 2+ values are just an NA and something else
-						# then just replace the NA with the something else
-						replacementsMade <- replacementsMade + 1L
-						Z[i,c(j):=list(unique(t.out[!is.na(t.out)]))]
-					}else{ # otherwise, if there are more than 2 non-NA unique values
-						prob.spp <- i # prob. means "problem" / "problematic"
-						prob.col <- j
-						prob.opts <- unique(t.out[!is.na(t.out)])
-				
-
-						fix.taxProb <- function(){ # defining in function in loop for readability
-							readline(paste(
-								"Pick your solution. SKIP to skip, otherwise enter one of the following exactly (don't add quotes, etc.):\n ", 
-								paste0(prob.opts, collapse="\n  "), "\n"
-							))
-						}
-				
-						if(tPS){ # FALSE; a prompt from when I had this in a script, was TRUE if reading in a file that had some answers
-							prob.fix.t <- taxProblemSolution[spp==i & prob.col==j,solution]
-							if(length(prob.fix.t)>0){
-								prob.fix <- prob.fix.t
-							}else{
-								print(j)
-								print(Z[i])
-								prob.fix <- fix.taxProb()
-								taxProblemSolution <- rbind(taxProblemSolution, data.table(spp=i, prob.col=j, solution=prob.fix))
-							}
-						}else{
-							print(j)
-							print(Z[i])
-							prob.fix <- fix.taxProb()
-							taxProblemSolution <- rbind(taxProblemSolution, data.table(spp=i, prob.col=j, solution=prob.fix))
-						}
-				
-				
-						if(prob.fix!="SKIP"){ # I've never tested the use of the SKIP response ...
-							Z[i,c(j):=list(prob.fix)]
-							replacementsMade <- replacementsMade + 1L
-						}else{
-							replacementsFailed <- replacementsFailed + 1L
-						}
-				
-					}
-				}else{
-					replacementUnneeded <- replacementUnneeded + 1L
-				}
-			}
 	
-		}
-	}
 	
 	
 	# ======================

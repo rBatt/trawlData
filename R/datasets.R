723ad6f3dda118740154470667ed63edac2f748d
rBatt <battrd@gmail.com>
2015-11-06 00:40:07 -0500

    use templates of OA surveys that have meta data; and bonus

--- R/datasets.R
+++ R/datasets.R
@@ -1,30 +1,50 @@
+# =================
+# = Raw Data Sets =
+# =================
 #' Raw Aleutian Islands
 #' 
 #' Raw data set for the Aleutian Islands bottom trawl survey
 #' 
 #' 
-#' \itemize{
-#' 	\item STRATUM. the statistical stratum of the haul
-#' 	\item LATITUDE. latitude of the haul
-#' 	\item LONGITUDE. longitude of the haul, in western hemisphere degrees (for lon > 0, do lon-360)
-#' 	\item STATION. the station ID for the haul
-#' 	\item YEAR. year of haul
-#' 	\item DATETIME. the day and time of the haul
-#' 	\item WTCPUE. weight (mass) of the catch
-#' 	\item NUMCPUE. number of individuals caught per hectare in the haul
-#' 	\item COMMON. the common name of the organism sampled
-#' 	\item SCIENTIFIC. species scientific name; Genus species
-#' 	\item SID. species identification number
-#' 	\item BOT_DEPTH. the maximum depth of the water at the location of the haul
-#' 	\item BOT_TEMP. water temperature at the bottom at the location of the haul
-#' 	\item SURF_TEMP. water temperature at the surface at the location of the haul
-#' 	\item VESSEL. vessel ID
-#' 	\item CRUISE. cruise ID
-#' 	\item HAUL. the integer haul number within a cruise
-#' 	\item Areakm2. the area of the statistical stratum (km2)
-#' }
+#' @templateVar stratum.name STRATUM
+#' @template stratum
+#' @templateVar lat.name LATITUDE
+#' @template lat
+#' @templateVar lon.name LONGITUDE
+#' @template lon
+#' @templateVar station.name STATION
+#' @template station
+#' @templateVar year.name YEAR
+#' @template year
+#' @templateVar datetime.name DATETIME
+#' @template datetime
+#' @templateVar wtcpue.name WTCPUE
+#' @template wtcpue
+#' @templateVar cntcpue.name NUMCPUE
+#' @template cntcpue
+#' @templateVar common.name COMMON
+#' @template common
+#' @templateVar spp.name SCIENTIFIC
+#' @template spp
+#' @templateVar SID.name SID
+#' @template SID
+#' @templateVar depth.name BOT_DEPTH
+#' @template depth
+#' @templateVar btemp.name BOT_TEMP
+#' @template btemp
+#' @templateVar stemp.name SURF_TEMP
+#' @template stemp
+#' @templateVar vessel.name VESSEL
+#' @template vessel
+#' @templateVar cruise.name CRUISE
+#' @template cruise
+#' @templateVar haul.name HAUL
+#' @template haul
+#' @templateVar stratumarea.name Areakm2
+#' @template stratumarea
 #' 
 #' @format A data.table
+#' 
 "raw.ai"
 
 
@@ -34,6 +54,542 @@
 #' 
 #' Raw data set for the Eastern Berring Sea bottom trawl survey
 #' 
+#' @templateVar stratum.name STRATUM
+#' @template stratum
+#' @templateVar lat.name LATITUDE
+#' @template lat
+#' @templateVar lon.name LONGITUDE
+#' @template lon
+#' @templateVar station.name STATION
+#' @template station
+#' @templateVar year.name YEAR
+#' @template year
+#' @templateVar datetime.name DATETIME
+#' @template datetime
+#' @templateVar wtcpue.name WTCPUE
+#' @template wtcpue
+#' @templateVar cntcpue.name NUMCPUE
+#' @template cntcpue
+#' @templateVar common.name COMMON
+#' @template common
+#' @templateVar spp.name SCIENTIFIC
+#' @template spp
+#' @templateVar SID.name SID
+#' @template SID
+#' @templateVar depth.name BOT_DEPTH
+#' @template depth
+#' @templateVar btemp.name BOT_TEMP
+#' @template btemp
+#' @templateVar stemp.name SURF_TEMP
+#' @template stemp
+#' @templateVar vessel.name VESSEL
+#' @template vessel
+#' @templateVar cruise.name CRUISE
+#' @template cruise
+#' @templateVar haul.name HAUL
+#' @template haul
+#' @templateVar stratumarea.name Areakm2
+#' @template stratumarea
+#' 
+#' @format A data.table
+"raw.ebs"
+
+
+
+
+#' Raw Gulf of Mexico
+#' 
+#' Raw data set for the Gulf of Mexico bottom trawl survey
+#' 
+#' @templateVar cruise.name CRUISEID
+#' @template cruise
+#' @templateVar vessel.name VESSEL
+#' @template vessel
+#' @templateVar SID.name BIO_BGS
+#' @template SID
+#' @templateVar station.name STATIONID
+#' @template station
+#' @templateVar cruise.name CRUISE_NO
+#' @template cruise
+#' @templateVar station.name P_STA_NO
+#' @template station
+#' @templateVar BGSID.name BGSID
+#' @template BGSID
+# #' @templateVar stratum.name CATEGORY" = "unknown",
+
+#' @templateVar genus.name GENUS_BGS
+#' @template genus
+#' @templateVar species.name SPEC_BGS
+#' @template species
+#' @templateVar BGSCODE.name BGSCODE
+#' @template BGSCODE
+
+#' @templateVar cnt.name CNTEXP
+#' @template cnt
+#' @templateVar weight.name SELECT_BGS
+#' @template weight
+
+# #' @templateVar tow ID.name INVRECID" = "tow ID",
+
+#' @templateVar gearsize.name GEAR_SIZE
+#' @template gearsize
+#' @templateVar geartype.name GEAR_TYPE
+#' @template geartype
+#' @templateVar meshsize.name MESH_SIZE
+#' @template meshsize
+# #' @templateVar cruise.name OP = "status of the tow; blank is good, so is a '.'; all else bad (see filedef.doc)",
+#' @templateVar duration.name MIN_FISH
+#' @template duration
+
+#' @templateVar timezone.name TIME_ZN
+#' @template timezone
+#' @templateVar time.name TIME_MIL
+#' @template time
+
+
+#' @templateVar lat.deg.name S_LATD
+#' @template lat.deg
+#' @templateVar lat.min.name S_LATM
+#' @template lat.min
+#' @templateVar lon.deg.name S_LOND
+#' @template lon.deg
+#' @templateVar lon.min.name S_LONM
+#' @template lon.min
+
+#' @templateVar depth2.name DEPTH_SSTA
+#' @template depth2
+#' @templateVar date.name MO_DAY_YR
+#' @template date
+
+#' @templateVar lat.deg.name E_LATD
+#' @template lat.deg
+#' @templateVar lat.min.name E_LATM
+#' @template lat.min
+#' @templateVar lon.deg.name E_LOND
+#' @template lon.deg
+#' @templateVar lon.min.name E_LONM
+#' @template lon.min
+
+
+#' @templateVar stemp.name TEMP_SSURF
+#' @template stemp
+#' @templateVar btemp.name TEMP_BOT
+#' @template btemp
+
+#' @templateVar towspeed.name VESSEL_SPD
+#' @template towspeed
+# #' @templateVar cruise.name COMSTAT"= "station comments",
+#' @templateVar spp.name TAXONOMIC
+#' @template spp
+# #' @templateVar cruise.name TITLE" = "survey name"
+#' 
+#' @format A data.table
+"raw.gmex"
+
+
+
+
+#' Raw Gulf of Alaska
+#' 
+#' Raw data set for the Gulf of Alaska bottom trawl survey
+#' 
+#' @templateVar stratum.name STRATUM
+#' @template stratum
+#' @templateVar lat.name LATITUDE
+#' @template lat
+#' @templateVar lon.name LONGITUDE
+#' @template lon
+#' @templateVar station.name STATION
+#' @template station
+#' @templateVar year.name YEAR
+#' @template year
+#' @templateVar datetime.name DATETIME
+#' @template datetime
+#' @templateVar wtcpue.name WTCPUE
+#' @template wtcpue
+#' @templateVar cntcpue.name NUMCPUE
+#' @template cntcpue
+#' @templateVar common.name COMMON
+#' @template common
+#' @templateVar spp.name SCIENTIFIC
+#' @template spp
+#' @templateVar SID.name SID
+#' @template SID
+#' @templateVar depth.name BOT_DEPTH
+#' @template depth
+#' @templateVar btemp.name BOT_TEMP
+#' @template btemp
+#' @templateVar stemp.name SURF_TEMP
+#' @template stemp
+#' @templateVar vessel.name VESSEL
+#' @template vessel
+#' @templateVar cruise.name CRUISE
+#' @template cruise
+#' @templateVar haul.name HAUL
+#' @template haul
+#' @templateVar stratumarea.name Areakm2
+#' @template stratumarea
+#' 
+#' @format A data.table
+"raw.goa"
+
+
+
+
+#' Raw Northeast US
+#' 
+#' Raw data set for the Northeast US bottom trawl survey
+#' 
+#' @templateVar stratum.name STRATUM
+#' @template stratum
+#' @templateVar SID.name SVSPP
+#' @template SID
+#' @templateVar year.name YEAR
+#' @template year
+# #' @templateVar SEASON" = "survey season (i.e., season = collection of adjacent months)",
+#' @templateVar lat.name LAT
+#' @template lat
+#' @templateVar lon.name LON
+#' @template lon
+#' @templateVar depth.name DEPTH
+#' @template depth
+#' @templateVar cruise.name CRUISE6
+#' @template cruise
+#' @templateVar station.name STATION
+#' @template station
+#' @templateVar weight.name BIOMASS
+#' @template weight
+#' @templateVar cnt.name ABUNDANCE
+#' @template cnt
+#' @templateVar spp.name SCINAME
+#' @template spp
+#' @templateVar stratumarea2.name Areanmi2
+#' @template stratumarea2
+#' 
+#' @format A data.table
+"raw.neus"
+
+
+
+
+#' Raw Newfoundland
+#' 
+#' Raw data set for the Newfoundland bottom trawl survey
+#' 
 #' 
 #' 
 #' @format A data.table
+"raw.newf"
+
+
+
+
+#' Raw South Atlantic
+#' 
+#' Raw data set for the US Southeast bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"raw.sa"
+
+
+
+
+#' Raw Southern Gulf of St. Lawrence
+#' 
+#' Raw data set for the Southern Gulf of St. Lawrence bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"raw.sgulf"
+
+
+
+
+#' Raw Scotian Shelf
+#' 
+#' Raw data set for the Scotian Shelf bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"raw.shelf"
+
+
+
+
+#' Raw West Coast Annual
+#' 
+#' Raw data set for the West Coast Annual bottom trawl survey
+#' 
+#' @templateVar haulid.name Trawl.Id
+#' @template haulid
+#' @templateVar spp.name Species
+#' @template spp
+#' @templateVar weight.name Haul.Weight..kg.
+#' @template weight
+# "Individual.Average.Weight..kg." = "average individual weight (weight/count)", 
+# "Survey" = "Indicates the survey time series within which a trawl sample was conducted; either the West Coast Slope Bottom Trawl Survey or the West Coast Slope/Shelf Bottom Trawl Survey", 
+#' @templateVar year.name Survey.Cycle
+#' @template year
+#' @templateVar vessel.name Vessel
+#' @template vessel
+# "Cruise.Leg" = "The West Coast Groundfish Bottom Trawl Surveys have been conducted coastwide from the U.S. / Canada border to a southern boundary that has expanded southward from Point Conception to the U.S. - Mexico border over time (see accompanying data annotations document).  The full north to south extent is traversed in two passes each survey season.  Each pass is divided into four (Slope survey) to five (Slope/Shelf combined survey) continuous segments separated by rest periods in ports along the coast.  Cruise leg indicates the sequential segment (1-5) in which a trawl operation was conducted.", 
+# "Trawl.Performance" = "All trawl operations are reviewed and rated as to the quality of their execution and their applicability to formal fishery assessment analysis products.  Trawl Performance indicates these determinations.  See the accompanying data annotations document for a more detailed description.", 
+#' @templateVar date.name Trawl.Date
+#' @template date
+#' @templateVar datetime.name Trawl.Start.Time
+#' @template datetime
+#' @templateVar lat.name Best.Latitude..dd.
+#' @template lat
+#' @templateVar lon.name Best.Longitude..dd.
+#' @template lon
+#"Best.Position.Type" = "The best trawl position (Best Latitude, Best Longitude) available has been provided in decimal degrees North Latitude and annotated with its type (Best Position Type).  Preference is given to any recorded gear position, usually an estimate of the onbottom midpoint: [(start_lat+end_lat)/2,  (start_lon+end_lon)/2].  If no acceptable onbottom gear position can be determined, a vessel position is provided, usually a similarly determined trawl midpoint estimate for the vessel.  If no acceptable vessel position can be determined, position data recorded for the defined station is provided.", 
+#' @templateVar depth.name Best.Depth..m.
+#' @template depth
+# #' @templateVar .name Best.Depth.Type" = "how depth was determined", 
+#' @templateVar duration.name Trawl.Duration..min.
+#' @template duration
+#' @templateVar areaswept.name Area.Swept.by.the.Net..hectares.
+#' @template areaswept
+#' @templateVar btemp.name Temperature.At.the.Gear..degs.C.
+#' @template btemp
+#' 
+#' @format A data.table
+"raw.wcann"
+
+
+
+
+#' Raw West Coast Triennial
+#' 
+#' Raw data set for the West Coast Triennial bottom trawl survey
+#' 
+#' @templateVar SID.name SPECIES_CODE
+#' @template SID
+#' @templateVar cruise.name CRUISEJOIN
+#' @template cruise
+#' @templateVar haul.name HAULJOIN
+#' @template haul
+#' @templateVar vessel.name VESSEL
+#' @template vessel
+#' @templateVar cruise.name CRUISE
+#' @template cruise
+#' @templateVar haul.name HAUL
+#' @template haul
+# #' @templateVar .name CATCHJOIN" = "database id for the catch record",
+#' @templateVar weight.name WEIGHT
+#' @template weight
+#' @templateVar cnt.name NUMBER_FISH
+#' @template cnt
+#' @templateVar haul.name HAUL_TYPE
+#' @template  haul
+# #' @templateVar .name PERFORMANCE" = "performance code of the haul",
+#' @templateVar time.name START_TIME
+#' @template time
+#' @templateVar duration.name DURATION
+#' @template duration
+# #' @templateVar .name DISTANCE_FISHED" = "distance fished", # in km
+#' @templateVar gearsize.name NET_WIDTH
+#' @template gearsize
+#' @templateVar stratum.name STRATUM
+#' @template stratum
+# #' @templateVar .name START_LATITUDE" = "latitude  at the start of the haul",
+# #' @templateVar .name END_LATITUDE" = "latitude  at the end of the haul",
+# #' @templateVar .name START_LONGITUDE" = "longitude  at the start of the haul",
+# #' @templateVar .name END_LONGITUDE" = "longitude at the end of the haul",
+#' @templateVar station.name STATIONID
+#' @template station
+#' @templateVar depth.name BOTTOM_DEPTH
+#' @template depth
+#' @templateVar stemp.name SURFACE_TEMPERATURE
+#' @template stemp
+#' @templateVar btemp.name GEAR_TEMPERATURE
+#' @template btemp
+#' @templateVar spp.name SPECIES_NAME
+#' @template spp
+#' @templateVar common.name COMMON_NAME
+#' @template common
+#' 
+#' @format A data.table
+"raw.wctri"
+
+
+# ===================
+# = Clean Data Sets =
+# ===================
+#' Clean Aleutian Islands
+#' 
+#' Clean data set for the Aleutian Islands bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.ai"
+
+
+
+
+#' Clean Eastern Berring Sea
+#' 
+#' Clean data set for the Eastern Berring Sea bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.ebs"
+
+
+
+
+#' Clean Gulf of Mexico
+#' 
+#' Clean data set for the Gulf of Mexico bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.gmex"
+
+
+
+
+#' Clean Gulf of Alaska
+#' 
+#' Clean data set for the Gulf of Alaska bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.goa"
+
+
+
+
+#' Clean Northeast US
+#' 
+#' Clean data set for the Northeast US bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.neus"
+
+
+
+
+#' Clean Newfoundland
+#' 
+#' Clean data set for the Newfoundland bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.newf"
+
+
+
+
+#' Clean South Atlantic
+#' 
+#' Clean data set for the US Southeast bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.sa"
+
+
+
+
+#' Clean Southern Gulf of St. Lawrence
+#' 
+#' Clean data set for the Southern Gulf of St. Lawrence bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.sgulf"
+
+
+
+
+#' Clean Scotian Shelf
+#' 
+#' Clean data set for the Scotian Shelf bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.shelf"
+
+
+
+
+#' Clean West Coast Annual
+#' 
+#' Clean data set for the West Coast Annual bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.wcann"
+
+
+
+
+#' Clean West Coast Triennial
+#' 
+#' Clean data set for the West Coast Triennial bottom trawl survey
+#' 
+#' 
+#' 
+#' @format A data.table
+"clean.wctri"
+
+
+
+# =================================
+# = Environmental/ Misc Data Sets =
+# =================================
+#' ETOPO Depth
+#' 
+#' Water depth gridded from the ETOPO data source
+#' 
+#' @format raster brick
+"ETOPO"
+
+#' HadISST Surface Temperatures
+#' 
+#' Surface temperatures from the HadISST data source. Includes terrestrial and aquatic. Gridded.
+#' 
+#' @format raster brick
+"HadISST"
+
+#' SODA
+#' Gridded SODA bottom temperatures  
+#' 
+#' From 1958 to 2008. Longitude: 200W to 20E. Latitude: 0N to 90N.  
+#' 
+#' Can be the full data set (\code{soda}), or the annual maximum or mean (\code{soda.annMax} and \code{soda.annMean}, respectively).
+#' 
+#' @format raster brick
+#' @name SODA
+NULL
+
+#' @rdname SODA
+"soda"
+
+#' @rdname SODA
+"soda.annMax"
+
+#' @rdname SODA
+"soda.annMean"
+
+
+
+# =======================
+# = Taxonomic Data Sets =
+# =======================

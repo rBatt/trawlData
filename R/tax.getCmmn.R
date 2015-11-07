b5f35bfe7f7f6c6727bea63ef1413587a2c49abe
rBatt <battrd@gmail.com>
2015-11-06 19:22:22 -0500

    update getCmmn + docs

--- R/tax.getCmmn.R
+++ R/tax.getCmmn.R
@@ -4,10 +4,16 @@
 #' 
 #' @param u.sppCorr character vector of Latin names to lookup
 #' 
-#' Uses taxize, but this is very slow sometimes.
+#' Uses rfishbase (this is new from original implementation), and only uses taxize if rfishbase fails to find a match (but taxize is very slow sometimes). Furture versions should use the sealifebase implementation of rfishbase, which I haven't figured out/ tested yet.
 #' 
 #' @return a data.table with two columns: the species name searched, and the common name
+#' 
+#' @example
+#' # sometimes it works well, sometimes not
+#' getCmmn(c("Paralichthys dentatus", "Pomatomus saltatrix","Gadus morhua"))
 getCmmn <- function(u.sppCorr){
+	library(rfishbase)
+	library(taxize)
 	
 	use.pb <- length(u.sppCorr)>1
 		
@@ -19,24 +25,34 @@ getCmmn <- function(u.sppCorr){
 	for(i in 1:length(u.sppCorr)){
 		
 		# Look up common names
-		t.spp.cmmn00 <- tryCatch( # first try looking in ncbi b/c gives english
+		t.spp.cmmn00 <- tryCatch(
 			{
-				ncbi.check <- sci2comm(u.sppCorr[i], db="ncbi", ask=FALSE, verbose=FALSE)[[1]][1][[1]]
-				stopifnot(!is.null(ncbi.check))
-				ncbi.check
-			}, 
-			error=function(cond){ # if ncbi fails, ...
-				tryCatch( # next try finding the common name in itis
-					{sci2comm(u.sppCorr[i], db="itis", ask=FALSE, verbose=FALSE)[[1]]},
-					error=function(cond){ # if ncbi and itis fail, ...
-						tryCatch( # next look in eol
-							{sci2comm(u.sppCorr[i], db="eol", ask=FALSE, verbose=FALSE)[[1]]},
-							error=function(cond){NA} # if all of ncbi and itis and eol fail, return NA
-						)
-					} # end 2nd error function
+				rfb.check <- sci_to_common(u.sppCorr[i])[[1]]
+				stopifnot(length(rfb.check)>0)
+				rfb.check
+			}, error=function(cond){
+				tryCatch( # first try looking in ncbi b/c gives english
+					{ncbi.check <- sci2comm(u.sppCorr[i], db="ncbi", ask=FALSE, verbose=FALSE)[[1]][1][[1]]
+						stopifnot(!is.null(ncbi.check))
+						ncbi.check
+					}, 
+					error=function(cond){ # if ncbi fails, ...
+						tryCatch( # next try finding the common name in itis
+							{sci2comm(u.sppCorr[i], db="itis", ask=FALSE, verbose=FALSE)[[1]]},
+							error=function(cond){ # if ncbi and itis fail, ...
+								tryCatch( # next look in eol
+									{sci2comm(u.sppCorr[i], db="eol", ask=FALSE, verbose=FALSE)[[1]]},
+									error=function(cond){NA} # if all of ncbi and itis and eol fail, return NA
+								)
+							} # end 3rd error function
+						) # end 3rd try catch
+					} # end 2nd error function 
 				) # end 2nd try catch
-			} # end 1st error function 
+			} # end 1st error function
 		) # end 1st try catch
+			
+		
+			
 	
 		# Remove non-english characters
 		t.spp.cmmn0 <- t.spp.cmmn00[grepl("[a-zA-Z]", t.spp.cmmn00)][1] # only match common names with english chars

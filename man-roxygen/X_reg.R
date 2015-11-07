37219ad86e76346e27405932937fa68128607b85
rBatt <battrd@gmail.com>
2015-11-06 22:35:42 -0500

    delete unused template, fix X_reg template

--- man-roxygen/X_reg.R
+++ man-roxygen/X_reg.R
@@ -1,2 +1,2 @@
 #' @param X a \code{data.table} containing trawl data
-#' @ reg a region; should be one of the following: \code{c("ai","ebs","gmex","goa", "neus", "newf", "sa", "sgulf", "shelf", "wcann", "wctri")}
+#' @param reg a region; should be one of the following: \code{c("ai","ebs","gmex","goa", "neus", "newf", "sa", "sgulf", "shelf", "wcann", "wctri")}

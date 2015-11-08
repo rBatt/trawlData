ee829ba3af793217f773ea24851b110f10960c56
rBatt <battrd@gmail.com>
2015-11-08 00:44:53 -0500

    add stringi, suggest raster

--- pkgBuild/devtools/dependencies.R
+++ pkgBuild/devtools/dependencies.R
@@ -4,9 +4,10 @@ devtools::use_package("data.table", type="Depends") # Basis for handling all dat
 devtools::use_package("LaF", "Imports") # For reading in fwf files
 devtools::use_package("bit64", "Imports") # Data class for long integers in data.table
 devtools::use_package("stringr", "Imports") # for rmWhite
-devtools::use_package("raster", "Imports") # for gridded environmental data sets
+devtools::use_package("stringi", "Imports") # for converting to ASCII
 devtools::use_package("PBSmapping", "Imports") # for calculating stratum areas
 
+devtools::use_package("raster", "Suggests") # for gridded environmental data sets
 # devtools::use_package("maptools", "Suggests") # for calculating stratum areas
 # devtools::use_package("Hmisc", "Suggests") # for calculating stratum areas
 devtools::use_package("taxize", "Suggests") # for updating taxonomy databases

62827e585ec3172eb56c4cb1cb630eaebb6cc0fa
rBatt <battrd@gmail.com>
2015-11-08 23:22:20 -0500

    better regex to get region name from files

--- pkgBuild/R/update_cleanData.R
+++ pkgBuild/R/update_cleanData.R
@@ -1,8 +1,9 @@
 
 
 update_cleanData <- function(reg, ...){	
-	
-	reg.p <- gsub("\\.RData", "", basename(reg), perl=T)	
+	message(paste0("received reg as: ",reg))
+	reg.p <- gsub("(\\.RData|\\.csv|raw\\.|-.*)", "", basename(reg[1]), perl=T)
+	message(paste0("stripped to ",reg.p))
 	
 	makeData(regions=reg.p, raw.load=T, clean=T)
 	
--- pkgBuild/R/update_rawData.R
+++ pkgBuild/R/update_rawData.R
@@ -2,7 +2,7 @@
 
 update_rawData <- function(reg, ...){	
 	
-	reg.p <- gsub("\\.zip", "", basename(reg), perl=T)
+	reg.p <- gsub("(\\.RData|\\.csv|raw\\.|-.*|\\.zip)", "", basename(reg[1]), perl=T)
 	
 	makeData(regions=reg.p, raw.read=T)
 	

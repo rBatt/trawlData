13434bc7cdcf82ec8bbf5b6471cd186d0749aa46
rBatt <battrd@gmail.com>
2015-11-13 19:31:13 -0500

    close to fully-prepared MSOM data

--- testFormat/trim_agg.R
+++ testFormat/trim_agg.R
@@ -1,10 +1,13 @@
+library(rbLib)
 
 
+ebs.t <- trawlTrim("ebs")
 
-ebs.trim <- trimData("ebs")
-
-ebs.agg1 <- aggData(
-	X = ebs.trim,
+# aggregate to make sure a haul
+# doesn't have duplicates of individuals
+# this aggregation uses the biological sum
+ebs.agg1 <- trawlAgg(
+	X = ebs.t,
 	bioFun = sumna,
 	envFun = meanna,
 	bio_lvl = "spp",
@@ -16,12 +19,19 @@ ebs.agg1 <- aggData(
 	meta.action = "unique1"
 )
 
+# drop new "time" column
+# change name of agg to indicate step
+# add a 1 and 1/10 degree grid
 ebs.agg1[,time_lvl:=NULL]
 setnames(ebs.agg1, "nAgg", "nAgg1")
 ebs.agg1[,stratum:=ll2strat(lon, lat, gridSize=1)]
 ebs.agg1[,aggStrat:=ll2strat(lon, lat, gridSize=0.1)]
 
 
+# aggregate at a 1/10 degree grid
+# this aggregation uses the biosum, too
+# NOTE: sum b/c I was targeting "paired" tows
+# should probably be mean after I close some Issues
 ebs.agg2 <- aggData(
 	X = ebs.agg1,
 	bioFun = sumna,
@@ -45,11 +55,11 @@ ebs.agg2 <- aggData(
 )
 
 
-
+# rename agg to indicate step
+# make a new haulid (combo of most specific time and space)
+# now haul id is day-year-(1/10 degree)
 setnames(ebs.agg2, "nAgg", "nAgg2")
-ebs.agg2[,haulid:=paste(aggStrat,time_lvl)] # haulid is defined as something within a 1/10th degree, same month of same year
-
-ebs.agg2[,stratum:=ll2strat(lon, lat, gridSize=1)]
+ebs.agg2[,haulid:=paste(aggStrat,time_lvl)]
 
 ebs.agg2[,j={
 	sll <- t(simplify2array(strsplit(unique(stratum), " ")))
@@ -59,11 +69,14 @@ ebs.agg2[,j={
 }]
 
 
-
 # ============
 # = Define K =
 # ============
-# Is region-year-stratum the right definition?
+# K is the number of haulid values in a reg-year-stratum
+# potential for K>1 because:
+#  finest temporal level is day
+#  finest spatial level is 1/10 degree (stratum is 1 degree)
+# Using "reg" is probably redundant with stratum in this case
 ebs.agg2[,K:=as.integer(as.factor(haulid)), by=c("reg","year","stratum")]
 ebs.agg2[,Kmax:=max(K), by=c("reg","year","stratum")]
 
@@ -71,6 +84,7 @@ ebs.agg2[,Kmax:=max(K), by=c("reg","year","stratum")]
 # ===================
 # = Plot K and Kmax =
 # ===================
+par(mfrow=auto.mfrow(ebs.agg2[,lu(reg)]))
 ebs.agg2[,j={
 		print(unique(year))
 		plot(.SD[,list(Kmax=mean(Kmax, na.rm=T)),keyby="year"], main=unique(reg), type="l")
@@ -81,11 +95,40 @@ ebs.agg2[,j={
 # plot((ebs.agg2[,list(Kmax=unique(Kmax)), by=c("reg","year","stratum")][,table(Kmax)]))
 
 (nKmax.reg <- ebs.agg2[,list(Kmax=unique(Kmax)), by=c("reg","year","stratum")][,table(Kmax, reg)])
-par(mfrow=c(4,3), mar=c(0.5,0.5,1,0), ps=9)
+par(mfrow=auto.mfrow(ebs.agg2[,lu(reg)]), mar=c(0.5,0.5,1,0), ps=9)
 for(i in 1:ncol(nKmax.reg)){
 	plot(nKmax.reg[,i], main=colnames(nKmax.reg)[i], type="h")
 }
 
 
+# ========
+# = Cast =
+# ========
+ebs.c <- trawlCast(ebs.agg2, stratum~K~spp~year, valueName="wtcpue")
 
+ebs.btemp.c <- trawlCast(ebs.agg2, 
+	stratum~K~year, 
+	valueName="btemp", 
+	fixAbsent=FALSE, 
+	fun.aggregate=meanna, 
+	valFill=NA_real_, 
+	grandNamesOut=c("stratum","K","year")
+)
 
+ebs.stemp.c <- trawlCast(ebs.agg2, 
+	stratum~K~year, 
+	valueName="stemp", 
+	fixAbsent=FALSE, 
+	fun.aggregate=meanna, 
+	valFill=NA_real_, 
+	grandNamesOut=c("stratum","K","year")
+)
+
+ebs.depth.c <- trawlCast(ebs.agg2, 
+	stratum~K~year, 
+	valueName="depth", 
+	fixAbsent=FALSE, 
+	fun.aggregate=meanna, 
+	valFill=NA_real_, 
+	grandNamesOut=c("stratum","K","year")
+)

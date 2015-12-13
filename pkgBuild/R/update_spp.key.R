82cf2e0a267c6ec20dca266c2c6f8ea92c6ca434
rBatt <battrd@gmail.com>
2015-12-12 19:34:24 -0500

    add ability to update spp.key when new raw data

--- pkgBuild/R/update_spp.key.R
+++ pkgBuild/R/update_spp.key.R
@@ -1,17 +1,43 @@
 
-update_spp.key <- function(...){
-	# Read in CSV
-	spp.key <- fread("inst/extdata/taxonomy/spp.key.csv", na.strings=c("","NA"))
-
-	# make sure encoding is ASCII
-	makeAsciiChar(spp.key)
-
-	# set data.table key (sorts)
-	setkey(spp.key, spp, ref)
+update_spp.key <- function(..., check_raw=FALSE){
 	
-	# save both .RData and .csv
-	save(spp.key, file="data/spp.key.RData")
-	write.csv(spp.key, file="inst/extdata/taxonomy/spp.key.csv", row.names=F)
+	# Check raw regional objects for new ref names
+	# ref is a column with spp names from raw data files
+	if(check_raw){
+		regs <- c("ai", "ebs", "gmex", "goa", "neus", "newf", "sa", "sgulf", "shelf", "wcann", "wctri")
 	
-	invisible(NULL)
+		raw_ref <- c()
+		for(i in 1:length(regs)){
+			clean.names(t_reg <- copy(get(paste0("raw.",regs[i]))), reg=regs[i])
+			clean.format(t_reg, regs[i])
+			raw_ref <- c(raw_ref, clean.tax(t_reg, regs[i])[,unique(ref)[!unique(ref)%in%raw_ref]])
+		}
+	
+		new_ref <- raw_ref[!raw_ref%in%spp.key[,ref] & raw_ref!="" & !is.na(raw_ref)]
+	
+		if(!length(new_ref)){
+			message("createSppKey has not found any new 'ref' in raw regional files; ")
+			return(NULL)
+		}else{
+			message("Updating spp.key with ", length(new_ref), " new 'ref' entries that createSppKey found in raw regional files")
+			createSppKey(new_ref, save.key=TRUE)
+		}
+	}else{
+		# Read in CSV
+		spp.key <- fread("inst/extdata/taxonomy/spp.key.csv", na.strings=c("","NA"))
+
+		# make sure encoding is ASCII
+		makeAsciiChar(spp.key)
+
+		# set data.table key (sorts)
+		setkey(spp.key, spp, ref)
+	
+		# save both .RData and .csv
+		save(spp.key, file="data/spp.key.RData")
+		write.csv(spp.key, file="inst/extdata/taxonomy/spp.key.csv", row.names=F)
+	
+		invisible(NULL)
+	}
+	
+
 }

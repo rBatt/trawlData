9da7c25414223cb805a930dca15cf82041d39d1d
rBatt <battrd@gmail.com>
2016-04-23 17:18:27 -0400

    finish code to save becca changes

--- pkgBuild/add_becca_sppkey_update.R
+++ pkgBuild/add_becca_sppkey_update.R
@@ -15,7 +15,19 @@ for(i in names(becca)){
 }
 b_ref <- becca[,ref]
 
+spp.key[ref%in%b_ref & (!is.na(spp)|!is.na(trophicLevel)|!is.na(common))] # make sure everything becca looked up was just NA across the board
+
 spp.key <- copy(trawlData::spp.key)
 spp.key <- spp.key[!ref%in%b_ref]
 
-rbind(becca, spp.key, fill=TRUE)
+spp.key <- rbind(becca, spp.key, fill=TRUE)
+
+# ---- resave ----
+makeAsciiChar(spp.key)
+
+# set data.table key (sorts)
+setkey(spp.key, spp, ref)
+
+# save both .RData and .csv
+save(spp.key, file="data/spp.key.RData")
+write.csv(spp.key, file="inst/extdata/taxonomy/spp.key.csv", row.names=F)
